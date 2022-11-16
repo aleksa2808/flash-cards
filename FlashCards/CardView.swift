e6b72e4b36ba98fc65fbf1cc895dbc784a0b0454
Aleksa Pavlovic <pavlovicaleksa@yahoo.com>
2022-11-16 18:51:49 +0100

    add swipable card stack

---
+++ FlashCards/CardView.swift
@@ -0,0 +1,121 @@
+//
+//  CardView.swift
+//  FlashCards
+//
+//  Created by Aleksa Pavlović on 16.11.22..
+//
+
+import SwiftUI
+
+struct CardView: View {
+    @State private var translation: CGSize = .zero
+    @State private var swipeStatus: CorrectWrong = .none
+    
+    private var card: Card
+    private var onRemove: (_ card: Card) -> Void
+    
+    private var thresholdPercentage: CGFloat = 0.5 // when the card has draged 50% the width of the screen in either direction
+    
+    private enum CorrectWrong: Int {
+        case correct, wrong, none
+    }
+    
+    init(card: Card, onRemove: @escaping (_ card: Card) -> Void) {
+        self.card = card
+        self.onRemove = onRemove
+    }
+    
+    /// What percentage of our own width have we swipped
+    /// - Parameters:
+    ///   - geometry: The geometry
+    ///   - gesture: The current gesture translation value
+    private func getGesturePercentage(_ geometry: GeometryProxy, from gesture: DragGesture.Value) -> CGFloat {
+        gesture.translation.width / geometry.size.width
+    }
+    
+    var body: some View {
+        GeometryReader { geometry in
+            VStack(alignment: .leading) {
+                 ZStack(alignment: self.swipeStatus == .correct ? .topLeading : .topTrailing) {
+                    if self.swipeStatus == .correct {
+                        Text("CORRECT")
+                            .font(.headline)
+                            .padding()
+                            .cornerRadius(10)
+                            .foregroundColor(Color.green)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke(Color.green, lineWidth: 3.0)
+                        ).padding(24)
+                            .rotationEffect(Angle.degrees(-45))
+                    } else if self.swipeStatus == .wrong {
+                        Text("WRONG")
+                            .font(.headline)
+                            .padding()
+                            .cornerRadius(10)
+                            .foregroundColor(Color.red)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke(Color.red, lineWidth: 3.0)
+                        ).padding(.top, 45)
+                            .rotationEffect(Angle.degrees(45))
+                    }
+                }
+                
+                HStack {
+                    VStack(alignment: .leading, spacing: 6) {
+                        Text(self.card.frontText)
+                            .font(.title)
+                            .bold()
+//                        Text(self.card.backText)
+//                            .font(.subheadline)
+//                            .bold()
+                    }
+                    Spacer()
+                }
+                .padding(.horizontal)
+            }
+            .padding()
+            .background(Color.white)
+            .cornerRadius(10)
+            .shadow(radius: 5)
+            .animation(.interactiveSpring(), value: self.translation)
+            .offset(x: self.translation.width, y: 0)
+            .rotationEffect(.degrees(Double(self.translation.width / geometry.size.width) * 25), anchor: .bottom)
+            .gesture(
+                DragGesture()
+                    .onChanged { value in
+                        self.translation = value.translation
+                        
+                        if (self.getGesturePercentage(geometry, from: value)) >= self.thresholdPercentage {
+                            self.swipeStatus = .correct
+                        } else if self.getGesturePercentage(geometry, from: value) <= -self.thresholdPercentage {
+                            self.swipeStatus = .wrong
+                        } else {
+                            self.swipeStatus = .none
+                        }
+                        
+                }.onEnded { value in
+                    // determine snap distance > 0.5 aka half the width of the screen
+                        if abs(self.getGesturePercentage(geometry, from: value)) > self.thresholdPercentage {
+                            self.onRemove(self.card)
+                        } else {
+                            self.translation = .zero
+                        }
+                    }
+            )
+        }
+    }
+}
+
+// 7
+struct CardView_Previews: PreviewProvider {
+    static var previews: some View {
+        CardView(card: Card(id: 1, frontText: "hrana", backText: "comida"),
+                 onRemove: { _ in
+                    // do nothing
+            })
+            .frame(height: 400)
+            .padding()
+    }
+}
--- FlashCards/ContentView.swift
+++ FlashCards/ContentView.swift
@@ -7,15 +7,81 @@
 
 import SwiftUI
 
+struct Card: Hashable, CustomStringConvertible {
+    var id: Int
+    
+    let frontText: String
+    let backText: String
+    
+    var description: String {
+        return "pera detlic"
+    }
+}
+
 struct ContentView: View {
+    /// List of cards
+    @State var cards: [Card] = [
+        Card(id: 1, frontText: "skola", backText: "escuela"),
+        Card(id: 2, frontText: "rec", backText: "palabra"),
+        Card(id: 3, frontText: "hrana", backText: "comida"),
+        Card(id: 4, frontText: "slusati", backText: "escuchar"),
+        Card(id: 5, frontText: "pevati", backText: "cantar"),
+        Card(id: 6, frontText: "sto", backText: "mesa"),
+        Card(id: 6, frontText: "putovati", backText: "viajar"),
+        Card(id: 6, frontText: "zivot", backText: "vida")
+    ]
+    
+    /// Return the CardViews width for the given offset in the array
+    /// - Parameters:
+    ///   - geometry: The geometry proxy of the parent
+    ///   - id: The ID of the current card
+    private func getCardWidth(_ geometry: GeometryProxy, id: Int) -> CGFloat {
+        let offset: CGFloat = CGFloat(cards.count - 1 - id) * 10
+        return geometry.size.width - offset
+    }
+    
+    /// Return the CardViews frame offset for the given offset in the array
+    /// - Parameters:
+    ///   - geometry: The geometry proxy of the parent
+    ///   - id: The ID of the current card
+    private func getCardOffset(_ geometry: GeometryProxy, id: Int) -> CGFloat {
+        return  CGFloat(cards.count - 1 - id) * 10
+    }
+    
+    private var maxID: Int {
+        return self.cards.map { $0.id }.max() ?? 0
+    }
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+            GeometryReader { geometry in
+                LinearGradient(gradient: Gradient(colors: [Color.init(#colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1)), Color.init(#colorLiteral(red: 0.05882352963, green: 0.180392161, blue: 0.2470588237, alpha: 1))]), startPoint: .bottom, endPoint: .top)
+                    .frame(width: geometry.size.width * 1.5, height: geometry.size.height)
+                    .background(Color.blue)
+                    .clipShape(Circle())
+                    .offset(x: -geometry.size.width / 4, y: -geometry.size.height / 2)
+                
+                VStack(spacing: 24) {
+                    ZStack {
+                        ForEach(self.cards, id: \.self) { card in
+                            Group {
+                                // Range Operator
+                                if (self.maxID - 3)...self.maxID ~= card.id {
+                                    CardView(card: card, onRemove: { removedCard in
+                                        // Remove that card from our array
+                                        self.cards.removeAll { $0.id == removedCard.id }
+                                    })
+                                    .animation(.spring(), value: self.cards.count)
+                                    .frame(width: self.getCardWidth(geometry, id: card.id), height: geometry.size.height / 2)
+                                    .offset(x: 0, y: geometry.size.height / 8 + self.getCardOffset(geometry, id: card.id))
+                                }
+                            }
+                        }
+                    }
+                    Spacer()
+                }
+            }
+        }.padding()
     }
 }
 
