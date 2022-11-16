a5bb9857986d8fba18e18ae78a8b8307a48b7239
Aleksa Pavlovic <pavlovicaleksa@yahoo.com>
2022-11-16 16:48:08 +0100

    Initial Commit

---
+++ FlashCards/FlashCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashCardsApp.swift
+//  FlashCards
+//
+//  Created by Aleksa Pavlović on 16.11.22..
+//
+
+import SwiftUI
+
+@main
+struct FlashCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
