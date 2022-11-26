8e3ff96ebca98113c7a79cd3f7ea9f5a1a25e8a0
Aleksa Pavlovic <pavlovicaleksa@yahoo.com>
2022-11-26 04:29:09 +0100

    add edit cancellation and refresh deck state after saving

--- FlashCards/ContentView.swift
+++ FlashCards/ContentView.swift
@@ -15,17 +15,17 @@ struct Card: Identifiable {
 
 let initialCards = [
     Card(frontText: "skola", backText: "escuela"),
-//    Card(frontText: "rec", backText: "palabra"),
-//    Card(frontText: "hrana", backText: "comida"),
-//    Card(frontText: "slusati", backText: "escuchar"),
-//    Card(frontText: "pevati", backText: "cantar"),
-//    Card(frontText: "sto", backText: "mesa"),
-//    Card(frontText: "putovati", backText: "viajar"),
-//    Card(frontText: "zivot", backText: "vida"),
-//    Card(frontText: "skola", backText: "escuela"),
-//    Card(frontText: "rec", backText: "palabra"),
-//    Card(frontText: "hrana", backText: "comida"),
-//    Card(frontText: "slusati", backText: "escuchar"),
+    Card(frontText: "rec", backText: "palabra"),
+    Card(frontText: "hrana", backText: "comida"),
+    Card(frontText: "slusati", backText: "escuchar"),
+    Card(frontText: "pevati", backText: "cantar"),
+    Card(frontText: "sto", backText: "mesa"),
+    Card(frontText: "putovati", backText: "viajar"),
+    Card(frontText: "zivot", backText: "vida"),
+    Card(frontText: "skola", backText: "escuela"),
+    Card(frontText: "rec", backText: "palabra"),
+    Card(frontText: "hrana", backText: "comida"),
+    Card(frontText: "slusati", backText: "escuchar"),
     //    Card(frontText: "pevati", backText: "cantar"),
     //    Card(frontText: "sto", backText: "mesa"),
     //    Card(frontText: "putovati", backText: "viajar"),
@@ -66,13 +66,28 @@ func createCardIDSets(cards: [Card]) -> [[Int]] {
     return cardIDSets
 }
 
+struct DeckState {
+    var cardIDSets: [[Int]]
+    var currentStage: Int
+    var cardsLearned: Int
+    
+    mutating func reset(cards: [Card]) {
+        self.cardIDSets = createCardIDSets(cards: cards)
+        self.currentStage = 0
+        self.cardsLearned = 0
+    }
+}
+
 struct ContentView: View {
     @State private var cards: [Card] = initialCards
-    // TODO: needs to be cleared when entering the deck editor and repopulated when exiting it
-    @State private var cardIDSets: [[Int]] = createCardIDSets(cards: initialCards)
-    @State private var currentStage: Int = 0
-    @State private var cardsLearned: Int = 0
+    @State private var deckState = DeckState(
+        cardIDSets: createCardIDSets(cards: initialCards),
+        currentStage: 0,
+        cardsLearned: 0
+    )
+    
     @State private var swipeCount: Int = 0
+    @State private var showEditor = false
     
     private func getCardOffset(_ geometry: GeometryProxy, position: Int) -> CGFloat {
         return CGFloat(position) * -10
@@ -83,146 +98,157 @@ struct ContentView: View {
     }
     
     private func resetDeck() {
-        self.cardIDSets = createCardIDSets(cards: self.cards)
-        self.currentStage = 0
-        self.cardsLearned = 0
+        deckState.reset(cards: self.cards)
     }
     
     var body: some View {
-        NavigationView {
-            ZStack {
-                GeometryReader { geometry in
-                    LinearGradient(gradient: Gradient(colors: [Color.init(#colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1)), Color.init(#colorLiteral(red: 0.05882352963, green: 0.180392161, blue: 0.2470588237, alpha: 1))]), startPoint: .bottom, endPoint: .top)
-                        .frame(width: geometry.size.width * 1.5, height: geometry.size.height)
-                        .background(Color.blue)
-                        .clipShape(Circle())
-                        .offset(x: -geometry.size.width / 4, y: -geometry.size.height / 2)
-                    
-                    VStack(spacing: 24) {
-                        VStack(spacing: 0) {
-                            Text("Progress")
-                                .font(.title)
-                                .bold()
-                                .padding()
-                                .frame(maxWidth: .infinity, alignment: .center)
-                            
-                            HStack(spacing:0) {
-                                ForEach(Array(self.cardIDSets.map{$0.count}.enumerated()), id: \.0) { stage, cardsInStage in
-                                    if self.cardsLearned != self.cards.count && stage == self.currentStage {
-                                        ZStack {
-                                            Text(String(cardsInStage))
-                                                .font(.title)
-                                                .padding()
-                                                .frame(maxWidth: .infinity, alignment: .top)
-                                                .overlay(
-                                                    RoundedRectangle(cornerRadius: 10)
-                                                        .strokeBorder(Color.yellow, lineWidth: 5)
-                                                )
-                                        }
-                                    } else {
-                                        // TODO: any way to do conditional modifiers?
+        ZStack {
+            GeometryReader { geometry in
+                LinearGradient(gradient: Gradient(colors: [Color.init(#colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1)), Color.init(#colorLiteral(red: 0.05882352963, green: 0.180392161, blue: 0.2470588237, alpha: 1))]), startPoint: .bottom, endPoint: .top)
+                    .frame(width: geometry.size.width * 1.5, height: geometry.size.height)
+                    .background(Color.blue)
+                    .clipShape(Circle())
+                    .offset(x: -geometry.size.width / 4, y: -geometry.size.height / 2)
+                
+                VStack(spacing: 24) {
+                    VStack(spacing: 0) {
+                        Text("Progress")
+                            .font(.title)
+                            .bold()
+                            .padding()
+                            .frame(maxWidth: .infinity, alignment: .center)
+                        
+                        HStack(spacing:0) {
+                            ForEach(Array(self.deckState.cardIDSets.map{$0.count}.enumerated()), id: \.0) { stage, cardsInStage in
+                                if self.deckState.cardsLearned != self.cards.count && stage == self.deckState.currentStage {
+                                    ZStack {
                                         Text(String(cardsInStage))
                                             .font(.title)
                                             .padding()
                                             .frame(maxWidth: .infinity, alignment: .top)
+                                            .overlay(
+                                                RoundedRectangle(cornerRadius: 10)
+                                                    .strokeBorder(Color.yellow, lineWidth: 5)
+                                            )
                                     }
-                                }
-                                if self.cardsLearned == self.cards.count {
-                                    Text(String(self.cardsLearned))
-                                        .font(.title)
-                                        .padding()
-                                        .frame(maxWidth: .infinity, alignment: .top)
-                                        .overlay(
-                                            RoundedRectangle(cornerRadius: 10)
-                                                .strokeBorder(Color.green, lineWidth: 5)
-                                        )
                                 } else {
-                                    Text(String(self.cardsLearned))
+                                    // TODO: any way to do conditional modifiers?
+                                    Text(String(cardsInStage))
                                         .font(.title)
                                         .padding()
                                         .frame(maxWidth: .infinity, alignment: .top)
                                 }
                             }
+                            if self.deckState.cardsLearned == self.cards.count && !self.cards.isEmpty {
+                                Text(String(self.deckState.cardsLearned))
+                                    .font(.title)
+                                    .padding()
+                                    .frame(maxWidth: .infinity, alignment: .top)
+                                    .overlay(
+                                        RoundedRectangle(cornerRadius: 10)
+                                            .strokeBorder(Color.green, lineWidth: 5)
+                                    )
+                            } else {
+                                Text(String(self.deckState.cardsLearned))
+                                    .font(.title)
+                                    .padding()
+                                    .frame(maxWidth: .infinity, alignment: .top)
+                            }
                         }
-                        .frame(maxWidth: .infinity)
-                        .background(Color.white)
-                        .cornerRadius(10)
-                        .shadow(radius: 5)
-                        
-                        Spacer()
-                        
-                        if self.cardsLearned == self.cards.count {
+                    }
+                    .frame(maxWidth: .infinity)
+                    .background(Color.white)
+                    .cornerRadius(10)
+                    .shadow(radius: 5)
+                    
+                    Spacer()
+                    
+                    if self.deckState.cardsLearned == self.cards.count {
+                        if !self.cards.isEmpty {
                             Text("Deck learned!")
                                 .font(.title)
                                 .bold()
                                 .foregroundColor(.green)
                         } else {
-                            ZStack {
-                                ForEach(Array(self.cardIDSets[self.currentStage].prefix(4).enumerated().reversed()), id: \.element) { i, cardID in
-                                    let card = self.cards[cardID]
-                                    CardView(card: card, showText: i == 0, onRemove: { correct in
-                                        // only purpose is to update animations. is there a better way?
-                                        self.swipeCount += 1
-                                        
-                                        let removedCardID = self.cardIDSets[self.currentStage].remove(at: 0)
-                                        if correct {
-                                            if self.currentStage == self.cardIDSets.count - 1 {
-                                                self.cardsLearned += 1
-                                            } else {
-                                                self.cardIDSets[self.currentStage + 1].append(removedCardID)
-                                            }
-                                        } else if self.currentStage > 0 {
-                                            self.cardIDSets[self.currentStage - 1].append(removedCardID)
-                                        } else {
-                                            self.cardIDSets[self.currentStage].append(removedCardID)
-                                        }
-                                        
-                                        if self.cardIDSets[self.currentStage].isEmpty {
-                                            if self.currentStage > 0 && !self.cardIDSets[self.currentStage - 1].isEmpty {
-                                                self.currentStage -= 1
-                                                self.cardIDSets[self.currentStage] = self.cardIDSets[self.currentStage].shuffled()
-                                            } else if self.currentStage < self.cardIDSets.count - 1 {
-                                                self.currentStage += 1
-                                                self.cardIDSets[self.currentStage] = self.cardIDSets[self.currentStage].shuffled()
-                                            }
-                                        }
-                                    })
-                                    .animation(.spring(), value: self.swipeCount)
-                                    .frame(width: self.getCardWidth(geometry, position: i), height: geometry.size.height / 2)
-                                    .offset(x: 0, y: self.getCardOffset(geometry, position: i))
-                                    
-                                }
-                            }
+                            Text("Deck empty!")
+                                .font(.title)
+                                .bold()
+                                .foregroundColor(.yellow)
                         }
-                        
-                        Spacer()
-                        
-                        HStack {
-                            NavigationLink(destination: StackEditorView(cards: $cards)) {
-                                Image(systemName: "pencil")
-                                    .font(.system(size: 42))
-                                    .bold()
-                                    .padding()
-                                    .background(Color.white)
-                                    .cornerRadius(40)
-                                    .foregroundColor(.blue)
-                                    .padding(10)
-                            }
-                            Button(action: {
-                                self.resetDeck()
-                            }) {
-                                Image(systemName: "arrow.clockwise")
-                                    .font(.system(size: 42))
-                                    .bold()
-                                    .padding()
-                                    .cornerRadius(40)
-                                    .foregroundColor(.blue)
-                                    .padding(10)
+                    } else {
+                        ZStack {
+                            ForEach(Array(self.deckState.cardIDSets[self.deckState.currentStage].prefix(4).enumerated().reversed()), id: \.element) { i, cardID in
+                                let card = self.cards[cardID]
+                                CardView(card: card, showText: i == 0, onRemove: { correct in
+                                    // TODO: simplify
+                                    
+                                    // only purpose is to update animations. is there a better way?
+                                    self.swipeCount += 1
+                                    
+                                    let removedCardID = self.deckState.cardIDSets[self.deckState.currentStage].remove(at: 0)
+                                    if correct {
+                                        if self.deckState.currentStage == self.deckState.cardIDSets.count - 1 {
+                                            self.deckState.cardsLearned += 1
+                                        } else {
+                                            self.deckState.cardIDSets[self.deckState.currentStage + 1].append(removedCardID)
+                                        }
+                                    } else if self.deckState.currentStage > 0 {
+                                        self.deckState.cardIDSets[self.deckState.currentStage - 1].append(removedCardID)
+                                    } else {
+                                        self.deckState.cardIDSets[self.deckState.currentStage].append(removedCardID)
+                                    }
+                                    
+                                    if self.deckState.cardIDSets[self.deckState.currentStage].isEmpty {
+                                        if self.deckState.currentStage > 0 && !self.deckState.cardIDSets[self.deckState.currentStage - 1].isEmpty {
+                                            self.deckState.currentStage -= 1
+                                            self.deckState.cardIDSets[self.deckState.currentStage] = self.deckState.cardIDSets[self.deckState.currentStage].shuffled()
+                                        } else if self.deckState.currentStage < self.deckState.cardIDSets.count - 1 {
+                                            self.deckState.currentStage += 1
+                                            self.deckState.cardIDSets[self.deckState.currentStage] = self.deckState.cardIDSets[self.deckState.currentStage].shuffled()
+                                        }
+                                    }
+                                })
+                                .animation(.spring(), value: self.swipeCount)
+                                .frame(width: self.getCardWidth(geometry, position: i), height: geometry.size.height / 2)
+                                .offset(x: 0, y: self.getCardOffset(geometry, position: i))
+                                
                             }
                         }
                     }
+                    
+                    Spacer()
+                    
+                    HStack {
+                        Button(action: {
+                            self.showEditor.toggle()
+                        }) {
+                            Image(systemName: "pencil")
+                                .font(.system(size: 42))
+                                .bold()
+                                .padding()
+                                .background(Color.white)
+                                .cornerRadius(40)
+                                .foregroundColor(.blue)
+                                .padding(10)
+                        }
+                        Button(action: {
+                            self.resetDeck()
+                        }) {
+                            Image(systemName: "arrow.clockwise")
+                                .font(.system(size: 42))
+                                .bold()
+                                .padding()
+                                .cornerRadius(40)
+                                .foregroundColor(.blue)
+                                .padding(10)
+                        }
+                    }
                 }
-            }.padding()
+            }
+        }
+        .padding()
+        .sheet(isPresented: $showEditor) {
+            StackEditorView(cards: $cards, onSave: resetDeck)
         }
     }
 }
--- FlashCards/StackEditorView.swift
+++ FlashCards/StackEditorView.swift
@@ -8,28 +8,81 @@
 import SwiftUI
 
 struct StackEditorView: View {
+    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
+    
+    @Binding var cardsBinding: [Card]
+    @State private var cards: [Card]
+    private var onSave: () -> Void
+    
+    @State private var newCard: Card = Card(frontText: "", backText: "")
+    
     enum Field: Hashable {
         case newCardFrontText
         case newCardBackText
     }
-    
-    @Binding var cards: [Card]
-    @State private var newCard: Card = Card(frontText: "", backText: "")
     @FocusState private var focusedField: Field?
     
     // TODO: using geometry size for these somehow moves the whole list down initially when it appears in a view with a navigation bar
     private let cardWidth = 150.0
     private let cardHeight = 100.0
     
+    init(cards: Binding<[Card]>, onSave: @escaping () -> Void = {}) {
+        self._cardsBinding = cards
+        self._cards = State(initialValue: cards.wrappedValue)
+        self.onSave = onSave
+    }
+    
     var body: some View {
-        List {
-            ForEach($cards) { $card in
+        NavigationView {
+            List {
+                ForEach($cards) { $card in
+                    HStack(spacing: 20) {
+                        Spacer()
+                        
+                        // TODO: use focused state to make touch area span the full card
+                        ZStack {
+                            TextField("", text: $card.frontText)
+                                .multilineTextAlignment(.center)
+                                .font(.system(size: 24))
+                                .bold()
+                        }
+                        .frame(width: self.cardWidth, height: self.cardHeight)
+                        .padding()
+                        .background(Color.white)
+                        .cornerRadius(10)
+                        .shadow(radius: 5)
+                        
+                        ZStack {
+                            TextField("", text: $card.backText)
+                                .multilineTextAlignment(.center)
+                                .font(.system(size: 24))
+                                .bold()
+                        }
+                        .frame(width: self.cardWidth, height: self.cardHeight)
+                        .padding()
+                        .background(Color.white)
+                        .cornerRadius(10)
+                        .shadow(radius: 5)
+                        
+                        Spacer()
+                    }
+                }
+                .onMove { indexSet, offset in
+                    cards.move(fromOffsets: indexSet, toOffset: offset)
+                }
+                .onDelete { indexSet in
+                    cards.remove(atOffsets: indexSet)
+                }
+                
                 HStack(spacing: 20) {
                     Spacer()
                     
-                    // TODO: use focused state to make touch area span the full card
                     ZStack {
-                        TextField("", text: $card.frontText)
+                        TextField("", text: $newCard.frontText)
+                            .onSubmit {
+                                self.focusedField = .newCardBackText
+                            }
+                            .focused($focusedField, equals: .newCardFrontText)
                             .multilineTextAlignment(.center)
                             .font(.system(size: 24))
                             .bold()
@@ -41,7 +94,17 @@ struct StackEditorView: View {
                     .shadow(radius: 5)
                     
                     ZStack {
-                        TextField("", text: $card.backText)
+                        TextField("", text: $newCard.backText)
+                            .onSubmit {
+                                if !newCard.frontText.isEmpty && !newCard.backText.isEmpty {
+                                    self.cards.append(newCard)
+                                    newCard = Card(frontText: "", backText: "")
+                                    self.focusedField = .newCardFrontText
+                                } else {
+                                    self.focusedField = nil
+                                }
+                            }
+                            .focused($focusedField, equals: .newCardBackText)
                             .multilineTextAlignment(.center)
                             .font(.system(size: 24))
                             .bold()
@@ -55,59 +118,26 @@ struct StackEditorView: View {
                     Spacer()
                 }
             }
-            .onMove { indexSet, offset in
-                cards.move(fromOffsets: indexSet, toOffset: offset)
-            }
-            .onDelete { indexSet in
-                cards.remove(atOffsets: indexSet)
-            }
-            
-            HStack(spacing: 20) {
-                Spacer()
-                
-                ZStack {
-                    TextField("", text: $newCard.frontText)
-                        .onSubmit {
-                            self.focusedField = .newCardBackText
-                        }
-                        .focused($focusedField, equals: .newCardFrontText)
-                        .multilineTextAlignment(.center)
-                        .font(.system(size: 24))
-                        .bold()
+            .listStyle(PlainListStyle())
+            .interactiveDismissDisabled()
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button("Cancel") {
+                        presentationMode.wrappedValue.dismiss()
+                    }
                 }
-                .frame(width: self.cardWidth, height: self.cardHeight)
-                .padding()
-                .background(Color.white)
-                .cornerRadius(10)
-                .shadow(radius: 5)
-                
-                ZStack {
-                    TextField("", text: $newCard.backText)
-                        .onSubmit {
-                            if !newCard.frontText.isEmpty && !newCard.backText.isEmpty {
-                                self.cards.append(newCard)
-                                newCard = Card(frontText: "", backText: "")
-                                self.focusedField = .newCardFrontText
-                            } else {
-                                self.focusedField = nil
-                            }
-                        }
-                        .focused($focusedField, equals: .newCardBackText)
-                        .multilineTextAlignment(.center)
-                        .font(.system(size: 24))
-                        .bold()
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button("Save") {
+                        // TODO: sanity checks
+                        
+                        self.cardsBinding = self.cards
+                        self.onSave()
+                        
+                        presentationMode.wrappedValue.dismiss()
+                    }
                 }
-                .frame(width: self.cardWidth, height: self.cardHeight)
-                .padding()
-                .background(Color.white)
-                .cornerRadius(10)
-                .shadow(radius: 5)
-                
-                Spacer()
             }
         }
-        .listStyle(PlainListStyle())
-        .navigationTitle("Deck")
     }
 }
 
